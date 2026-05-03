2446b8a9a85e8d5fb01748b9e839bbd8fafcaf3f
璃茉 <539959420@qq.com>
2026-05-03 13:19:00 +0800

    feat: add m12s2 config module and bump to 7.5.0.221

---
+++ BossMod/Modules/Dawntrail/Savage/M12SP2Lindwurm/M12S2LindwurmConfig.cs
@@ -0,0 +1,32 @@
+﻿namespace BossMod.Dawntrail.Savage.M12S2Lindwurm;
+
+[ConfigDisplay(Order = 0x160, Parent = typeof(DawntrailConfig))]
+public sealed class M12S2LindwurmConfig : ConfigNode
+{
+    public enum Replication1Strategy
+    {
+        [PropertyDisplay("Clone Relative")]
+        CloneRelative,
+
+        [PropertyDisplay("DN")]
+        DN
+    }
+
+    public enum Replication2Strategy
+    {
+        [PropertyDisplay("DN (true north)")]
+        DN,
+
+        [PropertyDisplay("Banana Codex (west = north)")]
+        BananaCodex
+    }
+
+    [PropertyDisplay("Replication 1 strategy")]
+    public Replication1Strategy Rep1Strategy = Replication1Strategy.CloneRelative;
+
+    [PropertyDisplay("Replication 2 strategy")]
+    public Replication2Strategy Rep2Strategy = Replication2Strategy.DN;
+
+    [PropertyDisplay("Show Replication 3 role hints")]
+    public bool ShowReplication3Hints = false;
+}
