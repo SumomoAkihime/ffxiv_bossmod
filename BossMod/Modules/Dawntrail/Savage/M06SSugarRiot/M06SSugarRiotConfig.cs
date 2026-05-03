ba5a782e53aaa98a60a01ea4e26d0393103e2609
璃茉 <539959420@qq.com>
2026-05-03 13:23:59 +0800

    feat: refine m06 priority metadata and bump to 7.5.0.223

--- BossMod/Modules/Dawntrail/Savage/M06SSugarRiot/M06SSugarRiotConfig.cs
+++ BossMod/Modules/Dawntrail/Savage/M06SSugarRiot/M06SSugarRiotConfig.cs
@@ -7,6 +7,8 @@ public sealed class M06SSugarRiotConfig() : ConfigNode
     public bool EnablePriorityList = true;
 
     [PropertyDisplay("Add phase priorities for autorotation (from highest to lowest)")]
+    [PropertyStringOrder(["Sugar Riot", "Mu P1", "Yan P1", "Gimme Cat P1", "Mu P2", "Feather Ray NW", "Feather Ray NE", "Yan P3", "Gimme Cat P3", "Jabberwock P3",
+    "Feather Ray SW", "Feather Ray SE", "Mu P4", "Gimme Cat P4", "Jabberwock P4", "Yan P4"])]
     public int[] AddsPriorityOrder = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
     [PropertyDisplay("Time left before spotlights get drawn")]
