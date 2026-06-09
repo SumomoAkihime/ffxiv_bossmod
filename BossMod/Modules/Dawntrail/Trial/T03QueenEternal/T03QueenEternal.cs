68137f45a99f2aae261591d22099cd003475dcbb
璃茉 <539959420@qq.com>
2026-06-09 12:23:42 +0800

    Register Dawntrail normal trial module aliases

--- BossMod/Modules/Dawntrail/Trial/T03QueenEternal/T03QueenEternal.cs
+++ BossMod/Modules/Dawntrail/Trial/T03QueenEternal/T03QueenEternal.cs
@@ -1,1 +1,11 @@
-using T03QueenEternal = BossMod.Dawntrail.Extreme.Ex3QueenEternal.Ex3QueenEternal;
+using Ex3AID = BossMod.Dawntrail.Extreme.Ex3QueenEternal.AID;
+using Ex3IconID = BossMod.Dawntrail.Extreme.Ex3QueenEternal.IconID;
+using Ex3Module = BossMod.Dawntrail.Extreme.Ex3QueenEternal.Ex3QueenEternal;
+using Ex3OID = BossMod.Dawntrail.Extreme.Ex3QueenEternal.OID;
+using Ex3SID = BossMod.Dawntrail.Extreme.Ex3QueenEternal.SID;
+using Ex3States = BossMod.Dawntrail.Extreme.Ex3QueenEternal.Ex3QueenEternalStates;
+
+namespace BossMod.Dawntrail.Trial.T03QueenEternal;
+
+[ModuleInfo(BossModuleInfo.Maturity.AISupport, StatesType = typeof(Ex3States), ObjectIDType = typeof(Ex3OID), ActionIDType = typeof(Ex3AID), StatusIDType = typeof(Ex3SID), IconIDType = typeof(Ex3IconID), PrimaryActorOID = (uint)Ex3OID.BossP1, Expansion = BossModuleInfo.Expansion.Dawntrail, Category = BossModuleInfo.Category.Trial, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 984, NameID = 13029)]
+public class T03QueenEternal(WorldState ws, Actor primary) : Ex3Module(ws, primary);
--- BossMod/Modules/Dawntrail/Trial/T05Necron/T05Necron.cs
+++ BossMod/Modules/Dawntrail/Trial/T05Necron/T05Necron.cs
@@ -1,1 +1,11 @@
-using T05Necron = BossMod.Dawntrail.Extreme.Ex5Necron.Ex5Necron;
+using Ex5AID = BossMod.Dawntrail.Extreme.Ex5Necron.AID;
+using Ex5IconID = BossMod.Dawntrail.Extreme.Ex5Necron.IconID;
+using Ex5Module = BossMod.Dawntrail.Extreme.Ex5Necron.Ex5Necron;
+using Ex5OID = BossMod.Dawntrail.Extreme.Ex5Necron.OID;
+using Ex5SID = BossMod.Dawntrail.Extreme.Ex5Necron.SID;
+using Ex5States = BossMod.Dawntrail.Extreme.Ex5Necron.Ex5NecronStates;
+
+namespace BossMod.Dawntrail.Trial.T05Necron;
+
+[ModuleInfo(BossModuleInfo.Maturity.AISupport, StatesType = typeof(Ex5States), ObjectIDType = typeof(Ex5OID), ActionIDType = typeof(Ex5AID), StatusIDType = typeof(Ex5SID), IconIDType = typeof(Ex5IconID), PrimaryActorOID = (uint)Ex5OID.Boss, Expansion = BossModuleInfo.Expansion.Dawntrail, Category = BossModuleInfo.Category.Trial, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1061, NameID = 14093)]
+public class T05Necron(WorldState ws, Actor primary) : Ex5Module(ws, primary);
